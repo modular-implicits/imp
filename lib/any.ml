cc7f909f1ecf0c4516b37084a1079fdd841af3c3
Patrick Reader <_@pxeger.com>
2023-08-04 17:38:09 +0100

    add Any instance for lists

--- lib/any.ml
+++ lib/any.ml
@@ -4,4 +4,5 @@ end
 
 implicit module Any_Int = struct type t_for_any = int end
 implicit module Any_String = struct type t_for_any = string end
-implicit module Any_Pair { A : Any } {B : Any } = struct type t_for_any = A.t_for_any * B.t_for_any end
+implicit module Any_List {A : Any} = struct type t_for_any = A.t_for_any list end
+implicit module Any_Pair {A : Any} {B : Any} = struct type t_for_any = A.t_for_any * B.t_for_any end
