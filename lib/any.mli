8532e20bd74a4741ca6abd0d4246657bda0ca1d4
Patrick Reader <_@pxeger.com>
2023-08-08 11:36:31 +0100

    add documentation for Any and Transformers

--- lib/any.mli
+++ lib/any.mli
@@ -1,6 +1,11 @@
 module type Any = sig
   type t_for_any
 end
+(** Any is an interface that all types can and should implement.
+    Note: the type is called t_for_any instead of t, because otherwise
+    many existing modules could accidentally fit the Any module type,
+    which would make implicit resolution ambiguous.
+    *)
 
 implicit module Any_Int : Any with type t_for_any = int
 implicit module Any_String : Any with type t_for_any = string
--- lib/transformers.mli
+++ lib/transformers.mli
@@ -2,10 +2,19 @@ open Any
 open Control
 
 type ('r, 'am) readerT
+(** The Reader and ReaderT monads.
+    For ReaderT, transforming an inner monad m, use ('r, 'a m) readerT
+    For Reader, with no inner monad, use ('r 'a) readerT
+ *)
 
 val runReaderT : {M : Monad} -> ('r, 'a M.t) readerT -> 'r -> 'a M.t
+(** Run a readerT monad using the given read-only state value *)
+
 val runReader : ('r, 'a) readerT -> 'r -> 'a
+(** Run a reader monad using the given read-only state value *)
+
 val lift : {M : Monad} -> 'a M.t -> ('r, 'a M.t) readerT
+(** Lift a value from the inner monad m to the transformed monad, ('r 'a m) readerT *)
 
 module type MonadReader = sig
   include Monad
@@ -13,10 +22,19 @@ module type MonadReader = sig
   val asks : (r -> 'b) -> 'b t
   val local : (r -> r) -> r t -> r t
 end
+(** MonadReader allows the functions ask, asks, and local
+    to be generalised to both reader and readerT
+    (both those types implement MonadReader)
+ *)
 
 val asks : {M : MonadReader} -> (M.r -> 'b) -> 'b M.t
+(** asks reads the state, applying a given transformation to it *)
+
 val local : {M : MonadReader} -> (M.r -> M.r) -> M.r M.t -> M.r M.t
+(** local runs the given reader monad using a transformed version of the state. *)
+
 val ask : {M : MonadReader} -> M.r M.t
+(** ask reads the state of the monad *)
 
 implicit module ReaderT {R : Any} {M : Monad} : sig
   include Functor with type 'a t = (R.t_for_any, 'a M.t) readerT
