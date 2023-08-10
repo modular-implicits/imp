34f5b5c47e8cd99d918c7d927e529cc8f79ed81f
Patrick Reader <_@pxeger.com>
2023-08-10 14:38:00 +0100

    wip

--- lib/transformers.ml
+++ lib/transformers.ml
@@ -57,3 +57,88 @@ end = struct
   let asks f = ReaderT f
   let local f (ReaderT g) = ReaderT (fun r -> g (f r))
 end
+
+type ('s, 'asm) stateT = StateT of ('s -> 'asm)
+
+let runStateT {M : Monad} (StateT f) = f
+let runState (StateT s) = s
+let lift {M : Monad} m = StateT (fun s -> M.fmap (fun x -> (s, x)) m)
+
+module type MonadState = sig
+  include Monad
+  type s
+  val get : s t
+  val put : s -> unit t
+end
+
+implicit module StateT {S : Any} {M : Monad} : sig
+  include Functor with type 'a t = (S.t_for_any, ('a * S.t_for_any) M.t) stateT
+  include Applicative with type 'a t := 'a t
+  include Monad with type 'a t := 'a t
+  include MonadState with type 'a t := 'a t and type s = S.t_for_any
+end = struct
+  type 'a t = (S.t_for_any, ('a * S.t_for_any) M.t) stateT
+  (* Functor *)
+  let fmap (f: 'a -> 'b) (StateT m: 'a t): 'b t = StateT (
+    fun s -> M.fmap (fun (x, s') -> (f x, s')) (m s)
+  )
+  (* Applicative *)
+  let return x = StateT (fun s -> M.return (x, s))
+  let apply (StateT ff) (StateT fx) = StateT (
+    fun s -> M.bind (ff s) @@
+    fun (f, s') -> M.bind (fx s') @@
+    fun (x, s'') -> M.return (f x, s'')
+  )
+  (* Monad *)
+  let bind (StateT fx) ff = StateT (
+    fun s -> M.bind (fx s) @@
+    fun (a, s') -> runStateT {M} (ff a) s'
+  )
+  (* MonadState *)
+  type s = S.t_for_any
+  let get = StateT (fun s -> M.return (s, s))
+  let put s = StateT (fun _ -> M.return ((), s))
+end
+
+let gets {M: MonadState} f = fmap f M.get
+let modify {M: MonadState} f = bind M.get (fun s -> M.put (f s))
+
+implicit module State {S : Any} : sig
+  include Functor with type 'a t = (S.t_for_any, 'a * S.t_for_any) stateT
+  include Applicative with type 'a t := 'a t
+  include Monad with type 'a t := 'a t
+  include MonadState with type 'a t := 'a t and type s = S.t_for_any
+end = struct
+  type 'a t = (S.t_for_any, 'a * S.t_for_any) stateT
+  (* Functor *)
+  let fmap (f: 'a -> 'b) (StateT m: 'a t): 'b t = StateT (
+    fun s ->
+      let (x, s') = m s in (f x, s')
+  )
+  (* Applicative *)
+  let return x = StateT (fun s -> (x, s))
+  let apply (StateT ff) (StateT fx) = StateT (
+    fun s ->
+      let (f, s') = ff s in
+      let (x, s'') = fx s' in
+      (f x, s'')
+  )
+  (* Monad *)
+  let bind (StateT fx) ff = StateT (
+    fun s -> let (a, s') = fx s in
+    runState (ff a) s'
+  )
+  (* MonadState *)
+  type s = S.t_for_any
+  let get = StateT (fun s -> (s, s))
+  let put s = StateT (fun _ -> ((), s))
+end
+
+module type MonadTrans = sig
+  module T : functor {M: Monad} -> Monad
+  val lift : {M: Monad} -> 'a M.t -> 'a TM.t
+end
+
+implicit module StateT_Trans {S: Any} : MonadTrans = struct
+  x
+end
